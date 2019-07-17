1a434a190f5dcee137d307ef8fb634d661b18c44
Manu <guyonnet@hotmail.com>
2019-07-17 19:25:42 +0200

    Remove dotenv

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,9 @@
-const path = require("path");
-require('dotenv').config();
-const mnemonic = process.env.MNENOMIC;
-const HDWalletProvider = require("truffle-hdwallet-provider");
+//const path = require("path");
+//require('dotenv').config();
+//const mnemonic = process.env.MNENOMIC;
+//const HDWalletProvider = require("truffle-hdwallet-provider");
 // Create your own key for Production environments (https://infura.io/)
-const INFURA_ID = 'd6760e62b67f4937ba1ea2691046f06d';
+//const INFURA_ID = 'd6760e62b67f4937wwwww2691046f06d';
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -13,7 +13,8 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
-    },
+    }
+    /*,
     ropsten: {
       provider: function() {
         return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY)
@@ -42,6 +43,6 @@ module.exports = {
       network_id: 1,
       gas: 3000000,
       gasPrice: 10000000000
-    }
+    }*/
   }
 };
