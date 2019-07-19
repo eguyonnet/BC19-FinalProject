8e3117f39b3a661f25899a2dd526217cd40f5e71
Manu <guyonnet@hotmail.com>
2019-07-19 19:05:10 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -13,8 +13,8 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
-    }
-    /*,
+    },
+    /*
     ropsten: {
       provider: function() {
         return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY)
@@ -44,5 +44,18 @@ module.exports = {
       gas: 3000000,
       gasPrice: 10000000000
     }*/
+  },
+  compilers: {
+    solc: {
+       version: "0.5.10",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
   }
 };
