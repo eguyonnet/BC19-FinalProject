17ec4ff0b57aac617d039951c9354b3d50b1db4f
Manu <guyonnet@hotmail.com>
2019-07-29 20:56:23 +0200

    Updates

--- client/src/components/Web3Info/index.js
+++ client/src/components/Web3Info/index.js
@@ -7,7 +7,7 @@ export default class Web3Info extends Component {
     const { networkId, networkType, currentAccount, balance, isMetaMask } = this.props;
     return (
       <div className={styles.web3}>
-        <div width="25%">
+        <div width="25%" align="left">
           <p>Network: <b>{networkId} - {networkType}</b></p>          
           <p>Using Metamask: <b>{isMetaMask ? 'YES' : 'NO'}</b></p>
         </div>
@@ -17,7 +17,7 @@ export default class Web3Info extends Component {
             <Blockie opts={{ seed: currentAccount, size: 15, scale: 3 }} />
           </div>
         </div>
-        <div width="25%">
+        <div width="25%" align="right">
           <p>ETH balance: <b>{balance}</b></p>
         </div>
       </div>
